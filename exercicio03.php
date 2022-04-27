dbb7265328fde0e6ae9c390b890206e0492e5aa5
jessyca-jca26 <jessyca.jca26@gmail.com>
2022-04-27 16:56:31 -0300

    Exercicio

---
+++ exercicio03.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio Php</title>
+</head>
+
+<body>
+    <h1>Exercicios 3</h1>
+    <hr>
+
+    <?php
+
+    $salario = 1000;
+
+    
+
+    
+    
+    
+
+
+
+
+    ?>
+
+
+
+
+
+
+
+</body>
+</html>
